b91bcacbb8f331c286f15eed6070f7bb7ad65861
Vidura Vijerathne <viduranox@gmail.com>
2022-10-22 19:59:50 +0530

    product listing ,

--- js/script.js
+++ js/script.js
@@ -29,3 +29,52 @@ function signUp(){
     alert("signUp")
     window.location.href = "/index.html";
 }
+
+function viewProduct(id){
+    const mainDiv = document.getElementById(id);
+    mainDiv.classList.remove('col-lg-4');
+    mainDiv.classList.add('col-lg-12');
+    mainDiv.focus();
+
+    const imageDiv = document.getElementById('product_img_'+id);
+    imageDiv.classList.add('d-none')
+
+    const carosal = document.getElementById('product_carosal_'+id);
+    carosal.classList.remove('d-none')
+
+    const shortDescription = document.getElementById('product_shortDescription_'+id)
+    shortDescription.classList.add('d-none')
+
+    const longDescription = document.getElementById('product_longDescription_'+id);
+    longDescription.classList.remove('d-none');
+
+    const purchasDetails = document.getElementById('product_purchasDetails_'+id);
+    purchasDetails.classList.remove('d-none')
+}
+
+function closeProduct(id){
+    const mainDiv = document.getElementById(id);
+    mainDiv.classList.remove('col-lg-12');
+    mainDiv.classList.add('col-lg-4');
+
+    const imageDiv = document.getElementById('product_img_'+id);
+    imageDiv.classList.remove('d-none')
+
+    const carosal = document.getElementById('product_carosal_'+id);
+    carosal.classList.add('d-none')
+
+    const shortDescription = document.getElementById('product_shortDescription_'+id)
+    shortDescription.classList.remove('d-none')
+
+    const longDescription = document.getElementById('product_longDescription_'+id);
+    longDescription.classList.add('d-none');
+
+    const purchasDetails = document.getElementById('product_purchasDetails_'+id);
+    purchasDetails.classList.add('d-none')
+
+}
+
+function toggleCart(){
+    document.querySelector('.sidecart').classList.toggle('open-cart');
+  }
+  
