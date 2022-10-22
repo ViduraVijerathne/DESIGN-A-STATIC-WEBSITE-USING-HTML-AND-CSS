6a1dfa6d0e728de37493346573d43da4bff71370
Vidura Vijerathne <viduranox@gmail.com>
2022-10-22 11:40:58 +0530

    user sign in sign up pages

--- js/script.js
+++ js/script.js
@@ -1,11 +1,31 @@
-function onmouseover_item(Pid){
-    const heart = document.getElementById('heart_'+Pid);
+function onmouseover_item(Pid) {
+    const heart = document.getElementById('heart_' + Pid);
     heart.classList.remove('d-none')
-    
+
 
 }
 
- function onmouseout_item(Pid){
-    const heart = document.getElementById('heart_'+Pid);
+function onmouseout_item(Pid) {
+    const heart = document.getElementById('heart_' + Pid);
     heart.classList.add('d-none')
- }
+}
+
+
+
+
+function navigationSign(id,id2){
+    widget  = document.getElementById(id);
+    widget.classList.toggle('d-none')
+
+    widget  = document.getElementById(id2);
+    widget.classList.toggle('d-none')
+}
+
+function signIn(){
+    alert("sign in")
+    window.location.href = "/index.html";
+}
+function signUp(){
+    alert("signUp")
+    window.location.href = "/index.html";
+}
